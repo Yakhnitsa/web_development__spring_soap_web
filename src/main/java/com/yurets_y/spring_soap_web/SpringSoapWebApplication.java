892bf39a3c480887b8ad60c63fb7df32f3e5eaac
Yuriy work-pc <transpmailbox@gmail.com>
2021-01-29 13:52:26 +0200

    Initial commit

---
+++ src/main/java/com/yurets_y/spring_soap_web/SpringSoapWebApplication.java
@@ -0,0 +1,13 @@
+package com.yurets_y.spring_soap_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSoapWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yurets_y/spring_soap_web/SpringSoapWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurets_y.spring_soap_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
